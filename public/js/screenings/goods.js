d0c8619c043cb4668fbfc3d1897ef0665c421aaa
zenghaiming <1458766296@qq.com>
2016-05-27 13:29:29 +0800

    global.scss

--- public/js/screenings/goods.js
+++ public/js/screenings/goods.js
@@ -4,6 +4,34 @@ define(function(require){
     console.log(config)
 
     $(function () {
+        var $article = $('.health_detail article');
+        var articleWidth = $article.width();
+        console.log(articleWidth)
+
+        $.each($article.find('img'),function (index,item) {
+            console.log($(item))
+            var img = $(this);
+            var realWidth;//真实的宽度
+            var realHeight;//真实的高度
+            //这里做下说明，$("<img/>")这里是创建一个临时的img标签，类似js创建一个new Image()对象！
+
+            $("<img/>").attr("src", $(img).attr("src")).load(function () {
+                realWidth = this.width;
+                realHeight = this.height;
+                console.log('realWidth:',realWidth)
+                //如果真实的宽度大于浏览器的宽度就按照100%显示
+                if (realWidth >= articleWidth) {
+                    $(img).css("width", "100%").css("height", "auto");
+                }
+                else {//如果小于浏览器的宽度按照原尺寸显示
+                    $(img).css("width", realWidth + 'px').css("height", realHeight + 'px');
+                }
+
+
+
+            })
+        })
+
         $('.pages').on('click','.prev,.next',function () {
             var page = $(this).attr('data-page'),
                 $self =$(this)
@@ -23,12 +51,12 @@ define(function(require){
                     success:function(json){
                         console.log(json)
                         var json = json,
-                            $list = $('.list ul')
+                            $list = $('#list_ask ul')
                         if(json.success){
                             var data = json.data.data,
                                 html = '',
-                            //currentPage = json.data.currentPage
-                                currentPage = json.data.currentPageDirectly,
+                                currentPage = json.data.currentPage,
+                                //currentPage = json.data.currentPageDirectly,
                                 pageCount = json.data.pageCount
 
                             console.log(currentPage)
@@ -38,6 +66,7 @@ define(function(require){
                             if($self.hasClass('next')){
                                 if(currentPage==pageCount){
                                     $self.addClass('disable')
+                                    $self.siblings().removeClass('disable')
                                 }else if(currentPage!=1){
                                     $self.attr('data-page',currentPage+1)
                                     $self.siblings().removeClass('disable').attr('data-page',currentPage-1)
@@ -48,6 +77,7 @@ define(function(require){
                             }else{
                                 if(currentPage==1){
                                     $self.addClass('disable')
+                                    $self.siblings().removeClass('disable')
                                 }else if(currentPage!=1){
                                     $self.attr('data-page',currentPage-1)
                                     $self.siblings().removeClass('disable').attr('data-page',currentPage+1)
@@ -66,6 +96,9 @@ define(function(require){
             }
 
         })
+        
+        
+        
     })
 
 })
