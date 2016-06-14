61cf0a17c2a86ca43f5c231083893dc26d5a18f3
zenghaiming <1458766296@qq.com>
2016-06-14 16:18:34 +0800

    响应式

--- client/js/common.js
+++ client/js/common.js
@@ -2,6 +2,8 @@ define(function(require, exports, module){
     require('lazyload');
     require('./login');
     var utils = require('./libs/utils.js');
+    var Swiper = require('./libs/swiper.jquery.umd');
+    console.log("Swiper:",Swiper)
     $(function () {
         var $body = $('body');
 
@@ -13,7 +15,7 @@ define(function(require, exports, module){
         $.each($nav_A,function (index,arr) {
             href = $(arr).attr('href');
             if(curPathname.search(href)!=-1){
-                $(arr).parent().addClass('on').siblings().removeClass()
+                $(arr).parent().addClass('on').siblings().removeClass('on')
             }
 
         });
@@ -54,6 +56,38 @@ define(function(require, exports, module){
             var time = utils.GetLoacalDateString(timestamp);
             $this.html(time);
         });
+        //
+
+
+        var winWidth = $(window).width();
+        var $nav = $('.nav').find('.wrapper')
+        if(winWidth<768){
+            //导航滑动
+            $nav.addClass('swiper-container');
+            var swiper = new Swiper('.swiper-container', {
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                slidesPerView: 4
+                // spaceBetween: 30
+            });
+        }else{
+            $nav.removeClass('swiper-container');
+        }
+        $(window).resize(function () {
+            winWidth = $(window).width();
+            if(winWidth<768){
+                //导航滑动
+                $nav.addClass('swiper-container');
+                var swiper = new Swiper('.swiper-container', {
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                    slidesPerView: 4
+                    // spaceBetween: 30
+                });
+            }else{
+                $nav.removeClass('swiper-container');
+            }
+        })
     })
 
     
--- handlers/index.handler.js
+++ handlers/index.handler.js
@@ -25,9 +25,9 @@ exports.index = function(req,res) {
 
     }else{
         res.render('index', {
-            title: '都安全首页',
-            keywords: '都安全首页关键字',
-            description: '都安全首页描述',
+            title: '都安全医疗',
+            keywords: '都安全,都安全医疗,都安全两性生殖健康,都安全宫颈癌筛查,都安全两性筛查,生殖感染检查,阴道微生态检查',
+            description: '都安全医疗是一家专注两性生殖健康的筛查诊疗平台，遵循国际筛查诊疗规范，致力于提供两性生殖感染、宫颈癌筛查和女性阴道微生态等筛查诊疗一站式全流程医疗服务。',
             get_goods_list:get_goods_list,
             get_department:get_department
         });
